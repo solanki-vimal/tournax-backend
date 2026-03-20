6befda71c620de843c2965f12f55ce86c1391930
Vimal Solanki <solanki-vimal@users.noreply.github.com>
2026-03-20 17:06:19 +0530

    Initial Spring Boot Tornax project setup

---
+++ src/main/java/com/vimal/tournax/TournaxApplication.java
@@ -0,0 +1,13 @@
+package com.vimal.tournax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournaxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vimal/tournax/TournaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vimal.tournax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
